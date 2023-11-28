9ca169e1c7229d9df0eeedd30a190c05eeb70cdc
Teodor Poshyvak <teodor.poshyvak.t@gmail.com>
2023-11-29 00:13:04 +0200

    add update_2 classes

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -9,25 +9,22 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.TimeZone;
+import java.util.regex.Pattern;
 
 
 @WebServlet("/time/*")
 public class TimeServlet extends HttpServlet {
 
     private TemplateEngine engine;
-    TimeUTC timeUTC;
+    String zonedDateTime;
 
     @Override
     public void init() throws ServletException {
         engine = new TemplateEngine();
-        timeUTC = new TimeUTC();
+        zonedDateTime = "";
         JavaxServletWebApplication jswa = JavaxServletWebApplication.buildApplication(this.getServletContext());
         WebApplicationTemplateResolver resolver = new WebApplicationTemplateResolver(jswa);
         resolver.setPrefix("/WEB-INF/template/");
@@ -44,13 +41,15 @@ public class TimeServlet extends HttpServlet {
 
         String cookieTime = "";
         Cookie[] cookies = req.getCookies();
-        for (Cookie cookie : cookies) {
-            if ("lastTimezone".equals(cookie.getName())) {
-                if (cookie.getValue().contains("UTC+") || cookie.getValue().contains("UTC-")) {
-                    Integer num = Integer.valueOf(cookie.getValue().substring(4));
-                    if (cookie.getValue().length() <= 6 && num <= 18) {
-                        cookieTime = cookie.getValue();
-                        break;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("lastTimezone".equals(cookie.getName())) {
+                    if (Pattern.compile("UTC\\+\\d+").matcher(cookie.getValue()).find() || Pattern.compile("UTC\\-\\d+").matcher(cookie.getValue()).find()) {
+                        Integer num = Integer.valueOf(cookie.getValue().substring(4));
+                        if (cookie.getValue().length() <= 6 && num <= 18) {
+                            cookieTime = cookie.getValue();
+                            break;
+                        }
                     }
                 }
             }
@@ -58,13 +57,13 @@ public class TimeServlet extends HttpServlet {
 
         String baseTimeUTC = ZonedDateTime.now(ZoneId.of(("UTC")))
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
-        if (!cookieTime.isEmpty()) {
+        if (cookieTime != null && !cookieTime.isEmpty()) {
             baseTimeUTC = ZonedDateTime.now(ZoneId.of((cookieTime)))
                     .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
         }
 
         Context context = new Context(req.getLocale(), Map.of("BaseTime", baseTimeUTC,
-                "timezone", timeUTC,
+                "timezone", zonedDateTime,
                 "lastTimezone", cookieTime));
 
         engine.process("UTCtime", context, resp.getWriter());
@@ -73,13 +72,13 @@ public class TimeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String param = req.getParameter("timezone").replace("%2B", "+");
+        String param = req.getParameter("timezone").replace("%2B", "+").replace(" ", "");
         if (param != null && !param.trim().isEmpty()) {
             Cookie cookie = new Cookie("lastTimezone", param.replace("%2B", "+"));
             cookie.setMaxAge(5);
             resp.addCookie(cookie);
-            timeUTC.setTime(ZonedDateTime.now(ZoneId.of(param))
-                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")));
+            zonedDateTime = ZonedDateTime.now(ZoneId.of(param))
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
         }
         resp.sendRedirect("/Servlet/time");
     }
--- src/main/java/org/example/TimezoneValidateFilter.java
+++ src/main/java/org/example/TimezoneValidateFilter.java
@@ -16,9 +16,9 @@ public class TimezoneValidateFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         try {
-            if (Objects.isNull(req.getPathInfo()) || req.getPathInfo().equals("/")) {
-                chain.doFilter(req, res);
-            }
+                if (Objects.isNull(req.getPathInfo()) || req.getPathInfo().equals("/")) {
+                    chain.doFilter(req, res);
+                }
         } catch (DateTimeException e) {
             res.setStatus(400);
             res.setContentType("text/html");
